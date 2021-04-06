9d38489f4181802d032970241c551d34709d49f8
Patrick Ware <patrick.r.ware@gmail.com>
2021-04-05 22:07:05 -0700

    icon removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { Icon } from "leaflet";
-import { BsFillCursorFill } from 'react-icons/bs';
 // Data
 import * as nationalParkData from "./data/national-parks.json";
 // Styles
@@ -10,11 +9,6 @@ import './App.css';
 function App() {
   const position = [39.8283, -98.5795]
   const [activePark, setActivePark] = useState(null);
-
-  const arrow = new Icon({
-    iconUrl: <BsFillCursorFill/>,
-    iconSize: [25, 25]
-  });
 
   return (
     <MapContainer center={position} zoom={5} scrollWheelZoom={false}>
