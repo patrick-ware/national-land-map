f8b58bd353f95217b5990806465a22daddcc5606
Patrick Ware <patrick.r.ware@gmail.com>
2021-04-24 13:22:48 -0700

    update import statements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { MapContainer, Marker, Polygon, Popup, TileLayer } from 'react-leaflet'
 import L from "leaflet";
 import useSwr from "swr";
