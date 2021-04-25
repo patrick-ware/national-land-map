283a69af933fbcd22e982802958a10685bb5fedd
Patrick Ware <patrick.r.ware@gmail.com>
2021-04-25 10:58:04 -0700

    basemap layer controls added

--- src/App.js
+++ src/App.js
@@ -57,11 +57,19 @@ function App() {
       <div>
         <MapContainer className="fade-in" center={center} zoom={zoom} scrollWheelZoom={false} whenCreated={setMap}>
         <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
         </LayersControl>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
           {nationalParkData.features.map(park => (
             <Marker
               icon={markerIcon}
